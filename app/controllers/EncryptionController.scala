99e97e51b9aa4b6f5c9748a29184c765025856f3
zoltanmaric <zoltanmaric@protonmail.com>
2016-10-15 23:42:51 +0200

    Encrypt, upload, download, decrypt, show file.

--- app/controllers/EncryptionController.scala
+++ app/controllers/EncryptionController.scala
@@ -1,9 +1,41 @@
 package controllers
 
-import play.api.mvc.{Action, Controller}
+import javax.inject.Inject
 
-class EncryptionController extends Controller {
+import play.api.libs.json._
+import play.api.mvc._
+import play.modules.reactivemongo._
+import reactivemongo.api.ReadPreference
+import reactivemongo.play.json._
+import reactivemongo.play.json.collection._
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class EncryptionController @Inject() (val reactiveMongoApi: ReactiveMongoApi)(implicit ec: ExecutionContext)
+  extends Controller with MongoController with ReactiveMongoComponents {
+
   def index = Action {
     Ok(views.html.encrypt())
   }
+
+  def upload = Action.async(parse.json) { request =>
+    photosCollection
+      .flatMap(_.insert(request.body.asInstanceOf[JsObject]))
+      .map(writeResult => Ok(s"Upload result: $writeResult"))
+  }
+
+  def download(filename: String) = Action.async {
+    photosCollection
+      .flatMap(findOneByFilename(filename))
+      .map(Ok(_))
+  }
+
+  private def findOneByFilename(filename: String)(photos: JSONCollection): Future[JsObject] =
+    photos.find(Json.obj("filename" -> filename))
+      .cursor[JsObject](ReadPreference.secondaryPreferred)
+      .collect[List]()
+      .map(_.head)
+
+  private def photosCollection: Future[JSONCollection] =
+    database.map(_.collection[JSONCollection]("photos"))
 }
--- public/javascripts/encrypt.js
+++ public/javascripts/encrypt.js
@@ -47,14 +47,60 @@ function handleFileSelect(evt) {
             armor: false                              // don't ASCII armor (for Uint8Array output)
         };
 
+        // Encrypt
         openpgp.encrypt(options).then(function (ciphertext) {
-            var encrypted = ciphertext.message.packets.write(); // get raw encrypted packets as Uint8Array
-            // console.log('encrypted: ' + JSON.stringify(encrypted));
-            document.getElementById("encryptedBase64textarea").value = encrypted;
-            return encrypted;
+            return ciphertext.message.packets.write(); // get raw encrypted packets as Uint8Array
         }).then(function (encrypted) {
+            // Convert to base64
+            var encryptedBase64 = btoa(String.fromCharCode.apply(null, encrypted));
+            console.log('encryptedBase64: ' + JSON.stringify(encryptedBase64));
+
+            document.getElementById("encryptedBase64textarea").value = encryptedBase64;
+            console.log("Encrypted length is " + encryptedBase64.length);
+
+            // Upload
+            return new Promise(function(resolve) {
+                var xhr = new XMLHttpRequest();
+                var url = "http://localhost:9000/upload";
+                xhr.open("POST", url, true);
+                xhr.setRequestHeader("Content-type", "application/json");
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState == 4 && xhr.status == 200) {
+                        console.log("Upload response: " + xhr.responseText);
+                        resolve();
+                    }
+                };
+                var data = {
+                    "filename": files[0].name,
+                    "encryptedData": encryptedBase64
+                };
+                console.log("Posting file " + data.filename);
+                xhr.send(JSON.stringify(data));
+            });
+        }).then(function () {
+            // Download
+            return new Promise(function (resolve) {
+                var xhr = new XMLHttpRequest();
+                var url = "http://localhost:9000/download?filename=" + files[0].name;
+                console.log("Downloading file " + url);
+                xhr.open("GET", url, true);
+                xhr.setRequestHeader("Content-type", "application/json");
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState == 4 && xhr.status == 200) {
+                        var json = JSON.parse(xhr.responseText);
+                        console.log("Downloaded: " + JSON.stringify(json, null, 2));
+                        resolve(json);
+                    }
+                };
+                xhr.send(null);
+            });
+        }).then(function (json) {
+            // Decrypt and show
+            console.log("Decrypting " + json.encryptedData);
+            var encryptedUint8Array = new Uint8Array(atob(json.encryptedData).split("").map(function(c) {
+                return c.charCodeAt(0); }));
             options = {
-                message: openpgp.message.read(encrypted), // parse encrypted bytes
+                message: openpgp.message.read(encryptedUint8Array), // parse encrypted bytes
                 password: 'secret stuff'                  // decrypt with password
                 // format: 'binary'                          // output as Uint8Array
             };
@@ -73,7 +119,7 @@ function handleFileSelect(evt) {
                 // now that we have the byte array, construct the blob from it
                 var blob1 = new Blob([byteArray], {type: "application/octet-stream"});
 
-                var fileName1 = "cool.jpg";
+                var fileName1 = json.filename;
                 saveAs(blob1, fileName1);
 
                 return plaintext.data; // Uint8Array([0x01, 0x01, 0x01])
